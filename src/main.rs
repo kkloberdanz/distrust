a577ff39e7780955951c7afd238f9d4ff737d40e
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-09-14 18:12:32 -0500

    cleaned up errors and formatted

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,12 @@
 
 use actix_files;
 use actix_web::{
-    web, App, HttpRequest, HttpResponse, Responder, error, Error, HttpServer
+    web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
 };
-use std::path::PathBuf;
-use std::cell::Cell;
+use futures::{Future, Stream};
 use std::fs;
 use std::io::Write;
-
-use actix_multipart::{Field, Multipart, MultipartError};
-use futures::future::{err, Either};
-use futures::{Future, Stream};
+use std::path::PathBuf;
 
 fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -53,32 +49,10 @@ fn get_file(req: HttpRequest) -> actix_web::Result<actix_files::NamedFile> {
     Ok(actix_files::NamedFile::open(path)?)
 }
 
-    
-//fn put_file(req: HttpRequest) -> actix_web::Result<String> {
-fn put_file(req: HttpRequest, payload: web::Payload) -> actix_web::Result<String> {
-    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
-    println!("saving to file: {:?}", path);
-    println!("req = {:#?}", req);
-    let file = fs::File::create(path)?;
-
-    //let response = format!("uploaded");
-    let data = payload.concat2();
-    /*
-    data
-        .fold(web::BytesMut::new(), move |mut body, chunk| {
-            body.extend_from_slice(&chunk);
-            Ok::<_, Error>(body)
-        })
-        .and_then(|body| {
-            format!("Body {:?}!", body);
-            Ok(HttpResponse::Ok().finish())
-        });
-    //println!("data = {:?}", data);
-    */
-    Ok("uploaded".to_string()) // HttpResponse::Ok().body(response)
-}
-
-fn test(req: HttpRequest, body: web::Payload) -> impl Future<Item = HttpResponse, Error = Error> {
+fn put_file(
+    req: HttpRequest,
+    body: web::Payload,
+) -> impl Future<Item = HttpResponse, Error = Error> {
     let path: PathBuf = req.match_info().query("filename").parse().unwrap();
     println!("saving to file: {:?}", path);
     println!("req = {:#?}", req);
@@ -91,7 +65,7 @@ fn test(req: HttpRequest, body: web::Payload) -> impl Future<Item = HttpResponse
             format!("Body {:?}!", body);
             println!("body = {:?}", body);
             let mut file = fs::File::create(path).unwrap();
-            file.write_all(&body);
+            file.write_all(&body).expect("failed to write");
             Ok(HttpResponse::Ok().finish())
         })
 }
@@ -115,12 +89,7 @@ fn main() {
             )
             .service(
                 web::scope("/put")
-                    .route("/{filename:.*}", web::put().to(put_file))
-                    //.route("/{filename:.*}", web::post().to_async(upload)),
-            )
-            .service(
-                web::scope("/test")
-                    .route("/{filename:.*}", web::put().to_async(test))
+                    .route("/{filename:.*}", web::put().to_async(put_file)),
             )
             .route("/", web::get().to(home))
     })
